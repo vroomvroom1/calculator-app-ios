39d537e0127ef43517120b57e684a1e7d1002589
Matt Vroom <mattvroom@Matts-MacBook-Air.local>
2017-09-30 17:00:55 -0400

    First commit

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,17 +9,67 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
+    //numbers
+    
+    @IBOutlet private weak var display: UILabel!
+    
+    private var userIsInTheMiddleOfTyping = false
+    
+    @IBAction private func touchDigit(_ sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        }
+        else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //additonal buttons
+    
+    private var displayValue: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set {
+            display.text = String(newValue)
+        }
     }
-
-
+    
+    private var brain = CalculatorBrain()
+    @IBAction private func performOperation(_ sender: UIButton) {
+        if userIsInTheMiddleOfTyping {
+            brain.setOperand(operand: displayValue)
+            userIsInTheMiddleOfTyping = false
+        }
+        
+        
+        if let mathSymbol = sender.currentTitle {
+            brain.performOperation(symbol: mathSymbol)
+            
+        }
+        displayValue = brain.result
+    }
+    
+    //clear button
+    
+    @IBAction func clearButton(_ sender: UIButton) {
+        
+        
+        if userIsInTheMiddleOfTyping == true{
+            display.text = "0"
+            
+            userIsInTheMiddleOfTyping = false
+        }
+        else {
+            display.text = ""
+        }
+        
+    }
+    
 }
 
